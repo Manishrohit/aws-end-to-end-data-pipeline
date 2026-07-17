64220818a56dcff67c67963aaf47a5385a0564a5
Manish kumar <130532705+Manishrohit@users.noreply.github.com>
2026-07-17 14:41:02 +0530

    Add AWS Glue PySpark ETL job

--- glue/glue_job.py
+++ glue/glue_job.py
@@ -1,1 +1,15 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
+spark = SparkSession.builder.appName('SalesETL').getOrCreate()
+
+# Read raw CSV
+df = spark.read.option('header', 'true').csv('s3://raw-sales-data/sales_data.csv')
+
+# Transform data
+df = df.withColumn('total_amount', col('quantity').cast('int') * col('unit_price').cast('int'))
+
+# Write transformed parquet
+df.write.mode('overwrite').parquet('s3://processed-sales-data/sales_data/')
+
+print('Glue ETL Job Completed')
